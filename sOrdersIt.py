83bab97b9f2e0cb6145c13a56693174a06d4b937
atrillini <152901272+atrillini@users.noreply.github.com>
2026-03-18 17:18:54 +0100

    Update sOrdersIt.py

--- sOrdersIt.py
+++ sOrdersIt.py
@@ -72,7 +72,7 @@ def getSOrdersFr():
     data = {
         'partner': LEROB_ID, 
         'date': target_date.strftime("%Y-%m-%d:00:00:00"),
-        'status': '11'
+        'status': '2'
     }
     res = requests.post(URL_FR, params=data)
     return xmltodict.parse(res.content)['root']['orders']
@@ -225,8 +225,8 @@ if(ordsFr) :
             
             for order in orders:
                 
-                #if(order['orders_id'] != '2000047567499648') :
-                    #continue
+                if(order['orders_id'] != '2000029062670748') :
+                    continue
                 
                        
                 products = order.get("products", {}).get("product", [])
